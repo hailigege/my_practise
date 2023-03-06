e0937eb794f226711a5049861684600adf5ee815
hlh <3145201178@qq.com>
2023-03-06 17:14:30 +0800

    first commit

---
+++ src/main/java/com/practise/DemoMSSpringbootApplication.java
@@ -0,0 +1,20 @@
+package com.practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName DemoMSSpringbootApplication
+ * @Description TODO
+ * @Author hlh
+ * @Date 2023/3/6 11:25
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class DemoMSSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMSSpringbootApplication.class, args);
+    }
+
+}
